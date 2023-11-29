61d778d5dc4b2c61aecbbda62875fd2ac80fd6d5
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-29 15:46:36 -0800

    added home get, started results

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def open_questions():
+    """open questions page as initial landing"""
+
+    return render_template(
+        'questions.html',
+        prompts=silly_story.prompts)
+
+@app.get('/results')
+def show_result_text():
+    """use form input to display text"""
