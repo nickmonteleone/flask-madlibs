2dfebc1e01b836291d1ac8909a1bce467ccfe744
Matt Smida <mcs@m4t7.com>
2023-11-29 19:16:24 -0500

    input and display of mad libs story

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story as story_instance
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -9,13 +9,20 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 @app.get('/')
-def open_questions():
+def show_prompts_form():
     """open questions page as initial landing"""
 
     return render_template(
         'questions.html',
-        prompts=silly_story.prompts)
+        prompts=story_instance.prompts)
 
 @app.get('/results')
-def show_result_text():
-    """use form input to display text"""
+def show_result_story():
+    """use form input to display story"""
+
+    story_to_display = story_instance.get_result_text(request.args)
+
+    return render_template(
+        'results.html',
+        story_to_display = story_to_display
+    )
