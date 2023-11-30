9fc92b91590a1b8ab01c240422eef42419568468
Matt Smida <mcs@m4t7.com>
2023-11-29 19:42:07 -0500

    a little docstring cleanup

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+""" This drives a webapp to present the user with a form to generate
+    a madlibs story. """
+
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -10,15 +13,16 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def show_prompts_form():
-    """open questions page as initial landing"""
+    """ Show a form to ask the user for inputs for the madlibs story. """
 
     return render_template(
         'questions.html',
-        prompts=story_instance.prompts)
+        prompts=story_instance.prompts
+    )
 
 @app.get('/results')
 def show_result_story():
-    """use form input to display story"""
+    """ Use form input to display madlibs story."""
 
     story_to_display = story_instance.get_result_text(request.args)
 
