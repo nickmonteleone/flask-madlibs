d56edfa3d71801b135fd4515134601cc4f026315
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-29 17:11:35 -0800

    added dropdown select for story

--- app.py
+++ app.py
@@ -4,26 +4,45 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story as story_instance
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+story_options = {
+    'Silly': silly_story,
+    'Excited': excited_story
+}
+
 @app.get('/')
+def show_story_select():
+    """ Show a dropdown for user to select which madlibs story to use """
+
+    return render_template(
+        'dropdown.html',
+        story_options=story_options.keys()
+    )
+
+@app.get('/prompts')
 def show_prompts_form():
     """ Show a form to ask the user for inputs for the madlibs story. """
 
+    story_option = request.args['story_options']
+    story_instance = story_options[story_option]
+
     return render_template(
         'questions.html',
-        prompts=story_instance.prompts
+        prompts=story_instance.prompts,
+        story_option=story_option
     )
 
-@app.get('/results')
-def show_result_story():
+@app.get('/results/<story_option>')
+def show_result_story(story_option):
     """ Use form input to display madlibs story."""
 
+    story_instance = story_options[story_option]
     story_to_display = story_instance.get_result_text(request.args)
 
     return render_template(
